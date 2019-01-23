53b10f7caac59912cff1f971f8f3385f025dd64f
freshield <zxdsw199182@gmail.com>
2019-01-23 17:37:50 +0800

    prepare for chp14

---
+++ chp14/MyDjango/index/urls.py
@@ -0,0 +1,25 @@
+#coding=utf-8
+"""
+@Author: Freshield
+@License: (C) Copyright 2018, BEIJING LINKING MEDICAL TECHNOLOGY CO., LTD.
+@Contact: yangyufresh@163.com
+@File: urls.py
+@Time: 2019-01-23 17:30
+@Last_update: 2019-01-23 17:30
+@Desc: None
+@==============================================@
+@      _____             _   _     _   _       @
+@     |   __|___ ___ ___| |_|_|___| |_| |      @
+@     |   __|  _| -_|_ -|   | | -_| | . |      @
+@     |__|  |_| |___|___|_|_|_|___|_|___|      @
+@                                    Freshield @
+@==============================================@
+"""
+from django.contrib import admin
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index),
+    path('index.html', views.index),
+]
